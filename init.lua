5b59107a87b317a90dd78036c90b0a4621abf5f4
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2024-11-07 14:58:47 -0500

    upload alacrity.yml and init.lua files

---
+++ init.lua
@@ -0,0 +1,12 @@
+vim.cmd([[
+	colorscheme retrobox
+	syntax on
+	set tabstop=2
+	set softtabstop=2
+	set shiftwidth=2
+	set autoindent
+	set smartindent
+	set expandtab
+  hi Normal guibg=NONE ctermbg=NONE
+]])
+
